23a68b50d9a73d0ad68e735815118d0a7e7526ff
jasontcrabtree <jasontcrabtree@gmail.com>
2024-01-05 12:33:34 +1300

    add index page msg

--- backend/Program.cs
+++ backend/Program.cs
@@ -64,6 +64,8 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () => Results.Json(new { Message = "Hello World!" }));
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
