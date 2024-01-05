1de66d68e4faf1d2d389d7ca7467d880104af585
jasontcrabtree <jasontcrabtree@gmail.com>
2024-01-05 15:21:40 +1300

    remove tw class to build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <div>
-      <h1>CRM Clone</h1>
+      <h1 >CRM Clone</h1>
       <Link href="/about">About</Link>
     </div>
   )
