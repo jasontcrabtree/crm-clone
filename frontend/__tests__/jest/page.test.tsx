ad8ee1e988f7c5f2453b3c63a4e1ea4c07553ea4
jasontcrabtree <jasontcrabtree@gmail.com>
2024-03-25 22:46:56 +1300

    remove old test

--- frontend/__tests__/jest/page.test.tsx
+++ frontend/__tests__/jest/page.test.tsx
@@ -1,17 +1,16 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import Page from '../../src/app/settings/page';
 
 test('making sure jest is configured properly', () => {
   expect(true).toBe(true);
 });
 
-describe('Page', () => {
-  it('renders a heading', () => {
-    render(<Page />);
+// describe('Page', () => {
+//   it('renders a heading', () => {
+//     render(<Page />);
 
-    const heading = screen.getByRole('heading', { level: 1 });
+//     const heading = screen.getByRole('heading', { level: 1 });
 
-    expect(heading).toBeInTheDocument();
-  });
-});
+//     expect(heading).toBeInTheDocument();
+//   });
+// });
