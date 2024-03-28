36620c392ca56d7039b7d962a7578b54a45f2959
jasontcrabtree <jasontcrabtree@gmail.com>
2024-03-29 11:35:18 +1300

    add default action slot page

---
+++ frontend/src/app/@actionslot/default.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+
+    return (
+        <div className="flex flex-col w-full h-full bg-yellow-500">
+            <h1 className="bg-whitex w-full text-xl text-purple-600 px-4 py-10 font-semibold">Default Fallback</h1>
+        </div>
+    )
+}
+
+export default Page;
